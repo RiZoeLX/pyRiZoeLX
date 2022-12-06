d0368d4419cbc7618a63b437fa6a3071612c3eb0
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-06 21:31:04 +0530

    Update get_user.py

--- RiZoeLX/functions/get_user.py
+++ RiZoeLX/functions/get_user.py
@@ -1,6 +1,6 @@
 
 
-asycy def user_and_reason(RiZoeL, message):
+async def user_and_reason(RiZoeL, message):
    args = ("".join(message.text.split(maxsplit=1)[1:])).split(" ", 2)
    if len(args) > 0:
       try:
@@ -26,7 +26,7 @@ asycy def user_and_reason(RiZoeL, message):
    return user, reason
 
 
-asycy def user_only(RiZoeL, message):
+async def user_only(RiZoeL, message):
    args = ("".join(message.text.split(maxsplit=1)[1:])).split(" ", 1)
    if args:
       try:
