6dc957361fd0cdfc193e4eca99e6e7b22672ae04
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-09 11:52:33 +0530

    Update admins.py

--- RiZoeLX/functions/admins.py
+++ RiZoeLX/functions/admins.py
@@ -6,35 +6,53 @@ import random
 from RiZoeLX.data import admin_tags
 from . import get_user_reason, get_user
 
+#Errors
+from pyrogram.errors import (ChatAdminRequired, RightForbidden, RPCError, UserNotParticipant)
+
+
 async def ban_user(RiZoeL, message):
    user, reason = await get_user_reason(RiZoeL, message)
+   try:
+      await RiZoeL.ban_chat_member(message.chat.id, user.id)
+   except ChatAdminRequired:
+      await message.reply_text("I'm not admin or I don't have rights.")
+      return
+   except RightForbidden:
+      await message.reply_text("I don't have enough rights to ban this user.")
+      return 
+   except UserNotParticipant:
+      await message.reply_text("How can I mute a user who is not a part of this chat?")
+      return 
+   except RPCError as eror:
+      await message.reply_text(str(eror))
+      return 
+
    if reason:
-     try:
-        await RiZoeL.ban_chat_member(message.chat.id, user.id)
-        await message.reply_text(f"Banned {user.mention}! \nReason: {reason}")
-     except Exception as eror:
-        await message.reply_text(str(eror))
+      await message.reply_text(f"Banned {user.mention}! \nReason: {reason}")
    else:
-     try:
-       await RiZoeL.ban_chat_member(message.chat.id, user.id)
-       await message.reply_text(f"Banned {user.mention}!")
-     except Exception as eror:
-       await message.reply_text(str(eror))
+      await message.reply_text(f"Banned {user.mention}!")
 
 async def unban_user(RiZoeL, message):
    user, reason = await get_user_reason(RiZoeL, message)
+   try:
+      await RiZoeL.unban_chat_member(message.chat.id, user.id)
+   except ChatAdminRequired:
+      await message.reply_text("I'm not admin or I don't have rights.")
+      return
+   except RightForbidden:
+      await message.reply_text("I don't have enough rights to ban this user.")
+      return 
+   except UserNotParticipant:
+      await message.reply_text("How can I mute a user who is not a part of this chat?")
+      return 
+   except RPCError as eror:
+      await message.reply_text(str(eror))
+      return 
+
    if reason:
-     try:
-       await RiZoeL.unban_chat_member(message.chat.id, user.id)
-       await message.reply_text(f"Unbanned {user.mention}! \nReason: {reason}")
-     except Exception as eror:
-       await message.reply_text(str(eror))
+      await message.reply_text(f"Unbanned {user.mention}! \nReason: {reason}")
    else:
-     try:
-       await RiZoeL.unban_chat_member(message.chat.id, user.id)
-       await message.reply_text(f"Unbanned {user.mention}!")
-     except Exception as eror:
-       await message.reply_text(str(eror))
+      await message.reply_text(f"Unbanned {user.mention}!")
 
 async def promote_user(RiZoeL, message):
   chat = message.chat.id
@@ -59,7 +77,13 @@ async def promote_user(RiZoeL, message):
             )
      await RiZoeL.set_administrator_title(chat.id, user.id, admin_tag)
      await message.reply_text(f"Promoted {user.mention}!")
-  except Exception as eror:
+  except ChatAdminRequired:
+     await message.reply_text("I'm not admin or I don't have rights.")
+  except RightForbidden:
+     await message.reply_text("I don't have enough rights to ban this user.")
+  except UserNotParticipant:
+     await message.reply_text("How can I mute a user who is not a part of this chat?")
+  except RPCError as eror:
      await message.reply_text(str(eror))
 
 async def promote_user(RiZoeL, message):
@@ -78,5 +102,57 @@ async def promote_user(RiZoeL, message):
             can_promote_members=False,
             )
      await message.reply_text(f"Demoted {user.mention}!")
-  except Exception as eror:
+  except ChatAdminRequired:
+     await message.reply_text("I'm not admin or I don't have rights.")
+  except RightForbidden:
+     await message.reply_text("I don't have enough rights to ban this user.")
+  except UserNotParticipant:
+     await message.reply_text("How can I mute a user who is not a part of this chat?")
+  except RPCError as eror:
      await message.reply_text(str(eror))
+
+
+from pyrogram.types import ChatPermissions
+
+async def mute_user(RiZoeL, message):
+   user, reason = await get_user_reason(RiZoeL, message)
+   try:
+      await RiZoeL.restrict_chat_member(message.chat.id, user.id, ChatPermissions())
+   except ChatAdminRequired:
+      await message.reply_text("I'm not admin or I don't have rights.")
+      return
+   except RightForbidden:
+      await message.reply_text("I don't have enough rights to ban this user.")
+      return 
+   except UserNotParticipant:
+      await message.reply_text("How can I mute a user who is not a part of this chat?")
+      return 
+   except RPCError as eror:
+      await message.reply_text(str(eror))
+      return
+   if reason:
+      await message.reply_text(f"Muted {user.mention}! \nReason: {reason}")
+   else:
+      await message.reply_text(f"Muted {user.mention}!")
+
+async def unmute_user(RiZoeL, message):
+   user, reason = await get_user_reason(RiZoeL, message)
+   try:
+      await message.chat.unban_member(user.id) 
+   except ChatAdminRequired:
+      await message.reply_text("I'm not admin or I don't have rights.")
+      return
+   except RightForbidden:
+      await message.reply_text("I don't have enough rights to ban this user.")
+      return 
+   except UserNotParticipant:
+      await message.reply_text("How can I mute a user who is not a part of this chat?")
+      return 
+   except RPCError as eror:
+      await message.reply_text(str(eror))
+      return
+   if reason:
+      await message.reply_text(f"Unuted {user.mention}! \nReason: {reason}")
+   else:
+      await message.reply_text(f"Unuted {user.mention}!")
+  
