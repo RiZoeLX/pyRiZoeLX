2a3673aeb5b3837fb69b81311a3c907e0c34708c
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-22 21:59:10 +0530

    Update usage_data.py

--- RiZoeLX/data/usage_data.py
+++ RiZoeLX/data/usage_data.py
@@ -3,7 +3,7 @@
 """
 
 class raid_usage:
-     raid = "**Wrong Usage** \n\nsyntax:\n {}raid (count) (username/id or reply to message) \n\n Example: `.raid 99 @Suzune_superbot`"
+     raid = "**Wrong Usage** \n\nsyntax:\n {}raid (count) (username/id or reply to user) \n\n Example: `.raid 99 @Suzune_superbot`"
           
 
 class spam_usage:
@@ -12,3 +12,11 @@ class spam_usage:
      delayspam = "**Wrong Usage** \n\nsyntax:\n {}delayspam (delay speed in seconds) (count) (message) \n\n Example: `.delayspam 0.5 99 hello`"
      
      pspam = "**Wrong Usage** \n\nsyntax:\n {}pornspam (count) \n\n Example: `.pornspam 99`"
+
+class dm_usage:
+     dm_spam = "**Wrong Usage** \n\nsyntax:\n {}dmspam (username/id or reply to message) (count) (message) \n\n Example: `.dmspam Suzune_superbot 99 hello`"
+     
+     dm_raid = "**Wrong Usage** \n\nsyntax:\n {}dmraid (count) (username/id or reply to user) \n\n Example: `.rmraid 99 @Suzune_superbot`"
+          
+     dm = "**Wrong Usage** \n\nsyntax:\n {}dm (username/id or reply to user) (message) \n\n Example: `.dm @Suzune_superbot hello`"
+          
