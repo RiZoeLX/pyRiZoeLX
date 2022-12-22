b3dfb80f351d8795d8f6d0606747374b5eff47fe
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-22 22:01:48 +0530

    Update __init__.py

--- RiZoeLX/__init__.py
+++ RiZoeLX/__init__.py
@@ -1,11 +1,10 @@
-
-
-from .developers import *
-from .version import *
-from RiZoeLX.resources.docs import *
-from RiZoeLX.functiona import *
-
-
 """
   RiZoeLX 2022 © pyRiZoeLX
 """
+
+from .developers import *
+from RiZoeLX.functions import *
+
+__version__ = "1.0.1"
+res_devs = "RiZoeL|Mahipal|Akash|TheRiZoeL|TheVenomXD"
+res_grps = [1517994352, 1789859817, -1001321613309, 1321613309, 1001749467927, 1749467927]
