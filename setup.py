51db29b63f4638058cfc86d2845ee3cef66d891c
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-25 16:10:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
-import re
-from setuptools import setup
+import re, codecs, sys, os
+from setuptools import setup, find_packages
 
 with open('RiZoeLX/version.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
@@ -8,20 +8,26 @@ with open('RiZoeLX/version.py') as f:
 if not version:
     raise RuntimeError('version is not set')
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-requirements = ['pyrogram', 'telethon', 'py-tgcalls']
+try:
+    with codecs.open( "README.md", 'r', errors='ignore' ) as file:
+        readme_contents = file.read()
+except Exception as error:
+    readme_contents = ""
+    sys.stderr.write( "Warning: Could not open README.md due %s\n" % error )
+    
+requirements = ['pyrogram', 'telethon', 'py-tgcalls', 'PhoenixScanner']
 
 setup(
     name='pyRiZoeLX',
     author='MrRiZoeL',
     author_email='xrizoel@gmail.com',
     version=version,
-    description='',
-    long_description=,
+    description='A Pyrogram based functional module.',
+    long_description = readme_contents,
+    long_description_content_type='text/markdown',
     url='https://github.com/RiZoeLX/pyRiZoeLX',
-    packages=['pyRiZoeLX'],
+    packages=find_packages('RiZoeLX'),
+    package_dir={'': 'RiZoeLX'},
     license='GNU General Public License v3.0',
     classifiers=[
         "Framework :: AsyncIO",
