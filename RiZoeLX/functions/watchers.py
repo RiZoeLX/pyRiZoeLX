9595f958f8ee8f8b4c44c053271f5825318f535c
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-18 10:59:51 +0530

    Update watchers.py

--- RiZoeLX/functions/watchers.py
+++ RiZoeLX/functions/watchers.py
@@ -37,25 +37,16 @@ Scanned by Team Red7 | Phoenix API ;)
 Appeal [Here](https://t.me/Red7WatchSupport)
    """
    try:
+      check = Red7_Client.check(user.id)
+   except Exception as eor:
+      print(str(eor))
+   
+   if check['is_gban']:
       try:
-         check = Red7_Client.check(user.id)
-         if check['is_gban']:
-            try:
-               await RiZoeL.ban_chat_member(message.chat.id, user.id)
-            except:
-               pass
-            await RiZoeL.send_message(message.chat.id, msg, disable_web_page_preview=True)
-      except:
-         SCANLIST = []
-         SCANLIST = update_scanlist(Red7_Client)
-         if user.id in SCANLIST:
-            try:
-               await RiZoeL.ban_chat_member(message.chat.id, user.id)
-            except:
-               pass
-            await RiZoeL.send_message(message.chat.id, msg, disable_web_page_preview=True)
-   except Exception as error:
-      print(str(error))
+         await RiZoeL.ban_chat_member(message.chat.id, user.id)
+         await RiZoeL.send_message(message.chat.id, msg, disable_web_page_preview=True)
+      except Exception as eorr:
+         print(str(eorr))
 
 
 # [Telethon]
@@ -83,24 +74,15 @@ Scanned by Team Red7 | Phoenix API ;)
 
 Appeal [Here](https://t.me/Red7WatchSupport)
    """
-   try:
       try:
-         check = Red7_Client.check(user)
-         if check['is_gban']:
-            try:
-                await RiZoeL(functions.channels.EditBannedRequest(message.chat_id, user, BANNED_RIGHTS))
-            except BaseException:
-                pass
-            await RiZoeL.send_message(message.chat_id, msg, link_preview=False)
-      except:
-         SCANLIST = []
-         SCANLIST = update_scanlist(Red7_Client)
-         if user in SCANLIST:
-            try:
-                await RiZoeL(functions.channels.EditBannedRequest(message.chat_id, user, BANNED_RIGHTS))
-            except BaseException:
-                pass
-            await RiZoeL.send_message(message.chat_id, msg, link_preview=False)
-   except Exception as error:
-      print(str(error))
+      check = Red7_Client.check(user.id)
+   except Exception as eor:
+      print(str(eor))
+   
+   if check['is_gban']:
+      try:
+         await RiZoeL(functions.channels.EditBannedRequest(message.chat_id, user, BANNED_RIGHTS))
+         await RiZoeL.send_message(message.chat_id, msg, link_preview=False)
+      except Exception as eorr:
+         print(str(eorr))
 
