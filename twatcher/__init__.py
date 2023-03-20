27242faa4fc6078712b24b691a2dfc7e85045a82
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-20 10:26:16 +0530

    Update __init__.py

--- twatcher/__init__.py
+++ twatcher/__init__.py
@@ -26,8 +26,7 @@ def random_token():
    token = random.choice(Toks)
    return token
 
-def update_scanlist():
-   Red7 = Phoenix(random_token())
+def update_scanlist(Red7):
    newlist = Red7.scanlist()
    if newlist == {'message': 'Invalid Token'}:
       newlist = Red7.scanlist()
@@ -37,48 +36,31 @@ def update_scanlist():
 
 async def Red7_Watch_telethon(RiZoeL, message):
    if message.user_joined or message.added_by:
-     user_id = message.sender_id
+     user = message.sender_id
+     Red7_Client = Phoenix(random_token())
      msg = f"""
-** Alert ⚠️**
-User [{user_id}](tg://user?id={user_id}) is officially
+ Alert ⚠️
+User [{user}](tg://user?id={user}) is officially
 Scanned by Team Red7 | Phoenix API ;)
 Appeal [Here](https://t.me/Red7WatchSupport)
      """
-     SCANLIST = update_scanlist()
-     if user_id in SCANLIST:
-       try:
-          await RiZoeL(functions.channels.EditBannedRequest(message.chat_id, user_id, BANNED_RIGHTS))
-          await RiZoeL.send_message(message.chat_id, msg, link_preview=False)
-       except Exception as eorr:
-          print(str(eorr))
-
-
-""" Some Telethon functions """
-async def get_user_telethon(message):
-    try:
-        args = message.text.split(" ", 1)[1].split(" ", 1)
-    except IndexError:
-        args = None
-    if message.reply_to_msg_id:
-        previous_message = await message.get_reply_message()
-        user = await message.client.get_entity(previous_message.sender_id)
-        extra = "".join(args) if args else ""
-    elif args:
-        extra = None
-        x = args[0]
-        if len(args) == 2:
-            extra = args[1]
-        if x.isnumeric():
-            x = int(x)
-        if not x:
-            await message.reply("I don't know who you're talking about, you're going to need to specify a user...!")
-            return
+     try:
         try:
-            x = await message.client.get_entity(x)
-        except (TypeError, ValueError):
-            await message.reply("Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them.")
-            return
-    else:
-        await message.reply("I don't know who you're talking about, you're going to need to specify a user...!")
-        return
-    return user, extra
+           check = Red7_Client.check(user)
+           if check['is_gban']:
+              try:
+                 await RiZoeL(functions.channels.EditBannedRequest(message.chat_id, user, BANNED_RIGHTS))
+                 await RiZoeL.send_message(message.chat_id, msg, link_preview=False)
+              except BaseException:
+                pass
+        except Exception:
+           SCANLIST = []
+           SCANLIST = update_scanlist(Red7_Client)
+           if user in SCANLIST:
+              try:
+                 await RiZoeL(functions.channels.EditBannedRequest(message.chat_id, user, BANNED_RIGHTS))
+                 await RiZoeL.send_message(message.chat_id, msg, link_preview=False)
+              except BaseException:
+                 pass
+     except Exception as error:
+        print(str(error))
